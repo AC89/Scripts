c9771d12c5d5f535ffb622beffe1d8ba8a6d60ea
Alex <alex@alexcoke.com>
2013-06-05 00:31:07 -0700

    Cleanup & new EOF style

--- batch/D3-Server_Test.bat
+++ batch/D3-Server_Test.bat
@@ -7,12 +7,12 @@ echo 3 - Test Asian Server
 echo 4 - Test All Servers
 echo 5 - EXIT
 echo.
-SET /P M=Select Test: 
-if %M%==1 goto NATEST
-if %M%==2 goto EUTEST
-if %M%==3 goto ASTEST
-if %M%==4 goto ALL
-IF %M%==5 exit
+choice /C 12345 /M "Select Test"
+if errorlevel 1 goto NATEST
+if errorlevel 2 goto EUTEST
+if errorlevel 3 goto ASTEST
+if errorlevel 4 goto ALL
+IF errorlevel 5 exit
 :NATEST
 Echo Starting PathPing
 Echo Starting D3-NA PathPing... >> "%USERPROFILE%\Desktop\D3-NA_PingPath.txt"
@@ -46,7 +46,7 @@ tracert 213.155.155.233 >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 Echo Finished TraceRoute >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 echo finished TraceRoute results are at %USERPROFILE%\Desktop\D3-EU_Traceroute.txt
 echo. >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
-GOTO EOF
+GOTO EUEOF
 :ASTEST
 Echo Starting PathPing
 Echo Starting D3-NA PathPing... >> "%USERPROFILE%\Desktop\D3-AS_PingPath.txt"
@@ -63,8 +63,19 @@ tracert 182.162.134.1 >> "%USERPROFILE%\Desktop\D3-AS_Traceroute.txt"
 Echo Finished TraceRoute >> "%USERPROFILE%\Desktop\D3-AS_Traceroute.txt"
 echo finished TraceRoute results are at %USERPROFILE%\Desktop\D3-AS_Traceroute.txt
 echo. >> "%USERPROFILE%\Desktop\D3-AS_Traceroute.txt"
-GOTO EOF
-:EOF
+GOTO ASEOF
+:NAEOF
 Echo All Jobs Finished, retest in 1 hour.
+Echo CTRL+C or close window to stop
 timeout /t 3600
-goto START
+GOTO NATEST
+:EUEOF
+Echo All Jobs Finished, retest in 1 hour.
+Echo CTRL+C or close window to stop
+timeout /t 3600
+GOTO EUTEST
+:ASEOF
+Echo All Jobs Finished, retest in 1 hour.
+Echo CTRL+C or close window to stop
+timeout /t 3600
+GOTO ASTEST
