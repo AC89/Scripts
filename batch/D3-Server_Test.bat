d2b09afe9ea66ca4de33de6c7f8f5b8cd83528c1
Alex <alex@alexcoke.com>
2013-06-05 00:50:52 -0700

    Clean up

--- batch/D3-Server_Test.bat
+++ batch/D3-Server_Test.bat
@@ -1,13 +1,16 @@
 @echo off
-TITLE Diablo 3 PingPath \ TraceRoute Utility
+TITLE Diablo 3 PathPing \ TraceRoute Utility
 :START
+Echo Diablo 3 PathPing and TraceRoute Utility
+Echo Written by AC89
+echo.
 echo 1 - Test North American Server
 echo 2 - Test Eruopian Server
 echo 3 - Test Asian Server
 echo 4 - Test All Servers
 echo 5 - EXIT
 echo.
-choice /C 12345 /M "Select Test"
+choice /C 12345 /N /M "Select Test: "
 if errorlevel 1 goto NATEST
 if errorlevel 2 goto EUTEST
 if errorlevel 3 goto ASTEST
@@ -22,8 +25,8 @@ Echo Finished PathPing >> "%USERPROFILE%\Desktop\D3-NA_PingPath.txt"
 echo Finished PathPing results are at %USERPROFILE%\Desktop\D3-NA_PingPath.txt
 echo. >> "%USERPROFILE%\Desktop\D3-NA_PingPath.txt"
 echo.
-Echo Starting Traceroute
-Echo Starting D3-NA TraceRoute... >> "%USERPROFILE%\Desktop\D3-NA_Traceroute.txt"
+Echo Starting trace route
+Echo Starting D3-NA trace route... >> "%USERPROFILE%\Desktop\D3-NA_Traceroute.txt"
 echo %time% - %date% >> "%USERPROFILE%\Desktop\D3-NA_Traceroute.txt"
 tracert 12.129.209.68 >> "%USERPROFILE%\Desktop\D3-NA_Traceroute.txt"
 Echo Finished TraceRoute >> "%USERPROFILE%\Desktop\D3-NA_Traceroute.txt"
@@ -39,13 +42,13 @@ Echo Finished PathPing >> "%USERPROFILE%\Desktop\D3-EU_PingPath.txt"
 echo Finished PathPing results are at %USERPROFILE%\Desktop\D3-EU_PingPath.txt
 echo. >> "%USERPROFILE%\Desktop\D3-EU_PingPath.txt"
 echo.
-Echo Starting Traceroute
+Echo Starting TraceRoute
 Echo Starting D3-NA TraceRoute... >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 echo %time% - %date% >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 tracert 213.155.155.233 >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 Echo Finished TraceRoute >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
 echo finished TraceRoute results are at %USERPROFILE%\Desktop\D3-EU_Traceroute.txt
-echo. >> "%USERPROFILE%\Desktop\D3-EU_Traceroute.txt"
+echo. >> "%USERPROFILE%\Desktop\D3-EU_Traceoute.txt"
 GOTO EUEOF
 :ASTEST
 Echo Starting PathPing
